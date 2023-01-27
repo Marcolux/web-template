e4eb72e332b99341cbe84d5cc5fac88b867f92b5
Marcolux <mccolucci1@gmail.com>
2023-01-27 14:04:44 -0600

    added menu templates

--- web-folder/script/main.js
+++ web-folder/script/main.js
@@ -1,2 +1,17 @@
 console.log('hello world');
 console.log('all the time');
+var colorLabel = '#ff9d00';
+var allTheRadiosNav = document.querySelectorAll('.inputNav');
+if (allTheRadiosNav) {
+    allTheRadiosNav.forEach(function (label) {
+        // const radio = label.children[0] as HTMLInputElement
+        // radio.checked ? label.style.backgroundColor = colorLabel : label.style.backgroundColor = 'none'
+        label.addEventListener('click', function () {
+            console.log(label.checked);
+        });
+        // radio.addEventListener('change', ()=>{
+        //     radio.checked ? label.style.backgroundColor = "#ff9d00" : label.style.backgroundColor = 'none';
+        //     radio.checked ? console.log(label.style.backgroundColor) : console.log(radio.checked);
+        // })
+    });
+}
--- web-folder/script/main.ts
+++ web-folder/script/main.ts
@@ -1,2 +1,23 @@
 console.log('hello world')
 console.log('all the time')
+
+let colorLabel = '#ff9d00' as string
+
+let allTheRadiosNav = document.querySelectorAll('.inputNav') as NodeListOf<HTMLInputElement>
+if(allTheRadiosNav){
+
+    allTheRadiosNav.forEach( label =>{
+    
+        // const radio = label.children[0] as HTMLInputElement
+        // radio.checked ? label.style.backgroundColor = colorLabel : label.style.backgroundColor = 'none'
+        label.addEventListener('click', ()=>{
+            console.log(label.checked);
+    
+        })
+        // radio.addEventListener('change', ()=>{
+        //     radio.checked ? label.style.backgroundColor = "#ff9d00" : label.style.backgroundColor = 'none';
+        //     radio.checked ? console.log(label.style.backgroundColor) : console.log(radio.checked);
+    
+        // })
+    })
+}
